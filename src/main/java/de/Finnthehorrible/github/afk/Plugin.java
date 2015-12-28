7d863f58c9d5ca74d7cd149be9866b112570840c
Finnthehorrible <isi220671@onlinehome.de>
2015-12-28 11:41:54 +0100

    Update Plugin.java

--- src/main/java/de/Finnthehorrible/github/afk/Plugin.java
+++ src/main/java/de/Finnthehorrible/github/afk/Plugin.java
@@ -2,6 +2,8 @@ package de.Finnthehorrible.github.afk;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Effect;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +11,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class Plugin implements CommandExecutor, Listener {
+    private Location location;
+
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = null;
         if(sender instanceof Player){
@@ -16,12 +20,13 @@ public class Plugin implements CommandExecutor, Listener {
 
         }
         if(cmd.getName().equalsIgnoreCase("afk")){
-                for (Player a : Bukkit.getOnlinePlayers()) {
-                    if(p != null) {
-                        a.sendMessage(ChatColor.DARK_BLUE + p.getName() + ChatColor.BLUE + "ist nun AFK(AwayFromKeyBord)");
-                    } else {
-                        a.sendMessage(ChatColor.DARK_BLUE + p.getName() + ChatColor.BLUE + "ist nun wieder da!");
-                    }
+            for (Player a : Bukkit.getOnlinePlayers()) {
+                if(p != null) {
+                    a.sendMessage(ChatColor.DARK_BLUE + p.getName() + ChatColor.BLUE + "ist nun AFK(AwayFromKeyBord");
+                    p.getWorld().playEffect(p.getLocation(), Effect.ENDER_SIGNAL, 3);
+                } else {
+                    a.sendMessage(ChatColor.DARK_BLUE + p.getName() + ChatColor.BLUE + "ist nun wieder da!");
+                }
             }
         }
         return false;
